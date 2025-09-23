38ae9e217320389d02690d48425f019a979364b4
learningpath2025 <learningpath2025@outlook.com>
2025-09-23 18:25:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Assignment5._4._1
+{
+    internal class Program
+    {
+        //With Recursion
+        public static void PrintIndividualNumber(int n)
+        {
+            string s = n.ToString();
+            int l = s.Length;
+
+            while (l > 0) 
+            {
+                int d = GetDivisor(l - 1);
+                Console.Write($"{n/d} ");
+                n %= d;
+                l--;
+            }
+        }
+
+        public static int GetDivisor(int l)
+        {
+            string s = "1";
+            while (l > 0)
+            {
+                s += Zero();
+                l--;
+            }
+            return Convert.ToInt32(s);
+        }
+
+        public static string Zero() { return "0"; }
+
+        //Without Recursion
+        public static void PrintIndividualNumber1(int n)
+        {
+            string x = n.ToString();
+            List<int> digits = x.Select(c => int.Parse(c.ToString())).ToList();
+            Console.Write(string.Join(" ", digits));
+        }
+        static void Main(string[] args)
+        {
+            int n = 1234565879;
+            PrintIndividualNumber(n);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Assignment5.4.1
+
+- Write a program in C# Sharp to display the individual digits of a given number using recursion.
+- Test Data :
+- Input any number : 1234
+- Expected Output :
+	- The digits in the number 1234 are : 1 2 3 4
+
+Hints:
+- Convert int to string and get the length.
+- Get the divisor for given number where Quotient will be the first digit and reminder will be rest of the integer.
+- Print the digit and repeat the process using recursion.
